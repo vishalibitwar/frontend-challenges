b97b2b0ea4a14af734a4708339d0554c962bad66
vishalibitwar <vish31ibitwar@gmail.com>
2020-05-12 21:53:40 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const projects = [
+  {
+    name: 'four-card-feature-section'
+  }
+];
+
+const color = ['cyan', 'magenta', 'lightgreen', 'blueviolet', 'orangered', 'aquamarine', 'crimson'];
+
+const list = document.getElementById('list');
+
+projects.forEach(({ name }, i) => {
+  const listItem = document.createElement('li');
+  listItem.style.borderBottomColor = color[i];
+
+  listItem.innerHTML = `
+		<a href="./${name}/index.html" target="_blank">
+			<img src="./${name}/design/desktop-design.jpg" alt="${name}" />
+			<p>${formatProjectName(name)}</p>
+		</a>
+	`;
+
+  list.appendChild(listItem);
+});
+
+function formatProjectName(name) {
+  return name.split('-').map(word => word[0].toUpperCase() + word.slice(1))
+    .join(' ');
+
+}
